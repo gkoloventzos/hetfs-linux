b9e2b74980c36ce7b4612c2c3014e3872e3154fd
Georgios Koloventzos <g.koloventzos@gmail.com>
2017-02-13 11:24:49 +0000

    adding METASLAB_ROTOR_VDEV types

--- include/linux/hetfs.h
+++ include/linux/hetfs.h
@@ -9,6 +9,22 @@
 #define MAX_DIFF 200000000
 #define MAX_NAME 255
 
+/*
+ * Five categories, from faster to slower:
+ *
+ * nonrot (SSD)      disk or mirror
+ * nonrot (SSD)      raidz
+ * mixed nonrot+rot  anything (raidz makes little sense)
+ * rot (HDD)         disk or mirror
+ * rot (HDD)         raidz
+ */
+
+#define	METASLAB_ROTOR_VDEV_TYPE_SSD		0x01
+#define	METASLAB_ROTOR_VDEV_TYPE_SSD_RAIDZ	0x02
+#define	METASLAB_ROTOR_VDEV_TYPE_MIXED		0x04
+#define	METASLAB_ROTOR_VDEV_TYPE_HDD		0x08
+#define	METASLAB_ROTOR_VDEV_TYPE_HDD_RAIDZ	0x10
+
 static DECLARE_RWSEM(tree_sem);
 
 struct analyze_request {
